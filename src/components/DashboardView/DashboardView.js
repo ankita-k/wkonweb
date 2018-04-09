29fa761a1575bff0cf00c78ccf4856a82498ddc9
Priyanka2095 <priyanka.saha@memeinfotech.com>
2018-04-09 12:33:20 +0530

    dashboard ui

--- src/components/DashboardView/DashboardView.js
+++ src/components/DashboardView/DashboardView.js
@@ -7,23 +7,59 @@ class DashboardView extends Component {
 
     render() {
         return (
-            // dashboardviewcustomer
-            <div className="dashboardView">
-                <h1>CUSTOMERS</h1>
-                <Row>
-                    <Col xs={24} sm={24} md={8} lg={8}>
-                        <div className="cusTotal">
-                            <p>
-                                <img src={total} alt="Customer" />Total
+            <div className="dashboardMain">
+                {/* dashboardviewcustomer */}
+                <div className="dashboardView">
+                    <h1>CUSTOMERS</h1>
+                    <Row>
+                        <Col xs={24} sm={24} md={8} lg={8}>
+                            <div className="cusTotal">
+                                <p>
+                                    <img src={total} alt="Customer" />Total
                         </p>
-                        <h1 class="totalNumber">1833</h1>
-                        </div>
-                    </Col>
-                    <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
-                    <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
-                </Row>
+                                <h1 class="totalNumber">1833</h1>
+                            </div>
+                        </Col>
+                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                    </Row>
+                </div>
+                {/* dashboardviewcustomer */}
+                {/* dashboardProject */}
+                <div className="dashboardView">
+                    <h1>CUSTOMERS</h1>
+                    <Row>
+                        <Col xs={24} sm={24} md={8} lg={8}>
+                            <div className="cusTotal">
+                                <p>
+                                    <img src={total} alt="Customer" />Total
+                        </p>
+                                <h1 class="totalNumber">1833</h1>
+                            </div>
+                        </Col>
+                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                    </Row>
+                </div>
+                {/* dashboardProject */}
+                {/* Recentactivity */}
+                <div className="dashboardView">
+                    <h1>CUSTOMERS</h1>
+                    <Row>
+                        <Col xs={24} sm={24} md={8} lg={8}>
+                            <div className="cusTotal">
+                                <p>
+                                    <img src={total} alt="Customer" />Total
+                        </p>
+                                <h1 class="totalNumber">1833</h1>
+                            </div>
+                        </Col>
+                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                    </Row>
+                </div>
+                {/* Recentactivity */}
             </div>
-            // dashboardviewcustomer
         );
     }
 }
