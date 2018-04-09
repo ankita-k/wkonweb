0aa2ee9deb81291880558e6f078645827dde2a52
payelduttameme <payel.dutta@memeinfotech.com>
2018-04-09 14:54:42 +0530

    code merged

--- src/components/DashboardView/DashboardView.js
+++ src/components/DashboardView/DashboardView.js
@@ -4,7 +4,11 @@ import './DashboardView.css';
 import total from '../../Images/total.png';
 import convert from '../../Images/convert.png';
 import pipeline from '../../Images/pipeline.png';
-
+import projecttotal from '../../Images/projecttotal.png';
+import progress from '../../Images/progress.png';
+import projectpipe from '../../Images/projectpipe.png';
+import man from '../../Images/wkon-2-21.png';
+import mantwo from '../../Images/wkon-2-22.png';
 class DashboardView extends Component {
 
     render() {
@@ -42,39 +46,66 @@ class DashboardView extends Component {
                     </Row>
                 </div>
                 {/* dashboardviewcustomer */}
-                {/* dashboardProject */}
-                <div className="dashboardView">
-                    <h1>CUSTOMERS</h1>
-                    <Row>
-                        <Col xs={24} sm={24} md={8} lg={8}>
-                            <div className="cusTotal">
-                                <p>
-                                    <img src={total} alt="Customer" />Total
-                        </p>
-                                <h1 class="totalNumber">1833</h1>
-                            </div>
-                        </Col>
-                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
-                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
-                    </Row>
-                </div>
-                {/* dashboardProject */}
+                 {/* Project section start */}
+
+                 <div className="ProjectSection">
+                 <h1 className="ProjectHead">PROJECTS</h1>
+                 <div className="ProjectDetails">
+                 <Row>
+                     <Col xs={24} sm={24} md={8} lg={8}>
+                         <div className="ProjectTotal">
+                         <p>
+                                 <img src={projecttotal} alt="Customer" /><span>Total</span>
+                         </p>
+                         <h1 class="ProjectNumber">300</h1>
+                         </div>
+                     </Col>
+                     <Col xs={24} sm={24} md={8} lg={8}>
+                      <div className="ProjectTotal">
+                      <p>
+                                 <img src={progress} alt="Customer" /><span>In Progress</span>
+                         </p>
+                         <h1 class="ProjectNumber">250</h1>
+                         </div>
+                         </Col>
+                     <Col xs={24} sm={24} md={8} lg={8}>
+                      <div className="ProjectTotal">
+                      <p>
+                                 <img src={projectpipe} alt="Customer" /><span>Pipeline</span>
+                         </p>
+                         <h1 class="ProjectNumber">200</h1>
+                         </div>
+                         </Col>
+                 </Row>
+                 </div>
+                 </div>
+ 
+                  {/* Project section end */}
                 {/* Recentactivity */}
-                <div className="dashboardView">
-                    <h1>CUSTOMERS</h1>
+                <div className="recentactivity">
+                <h1>RECENT ACTIVITY</h1>
+
+                <div className="firstman">
                     <Row>
-                        <Col xs={24} sm={24} md={8} lg={8}>
-                            <div className="cusTotal">
-                                <p>
-                                    <img src={total} alt="Customer" />Total
-                        </p>
-                                <h1 class="totalNumber">1833</h1>
-                            </div>
-                        </Col>
-                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
-                        <Col xs={24} sm={24} md={8} lg={8}>Col</Col>
+                        <Col lg={4}>
+                            <img src={man} /></Col>
+                        <Col lg={12}>
+                            <p>Mr. Stacey R.Eshelman our recently converted customer.</p></Col>
+                        <Col lg={6}><p className="date">09.04.2018</p></Col>
                     </Row>
                 </div>
+
+                <div className="secondman">
+                    <Row>
+                        <Col lg={4}>
+                            <img src={mantwo} /></Col>
+                        <Col lg={12}>
+                            <p>Mr.Felix D.Navarro in the pipeline customer</p></Col>
+                        <Col lg={6}><p className="date">01.03.2018</p></Col>
+                    </Row>
+                </div>
+
+            </div>
                 {/* Recentactivity */}
             </div>
         );
