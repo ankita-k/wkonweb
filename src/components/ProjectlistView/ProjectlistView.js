ede195ff02396d50637e703241ccc0fcadbe6255
Samikshameme <samiksha.sonam@memeinfotech.com>
2018-04-16 14:44:52 +0530

    Projectlist status

--- src/components/ProjectlistView/ProjectlistView.js
+++ src/components/ProjectlistView/ProjectlistView.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Card, Table, Button, Icon, Row } from 'antd';
 import { connect } from "react-redux";
 import * as actioncreators from '../../redux/action';
+import { Select } from 'antd';
 import '../ClientList/ClientList.css';
+const Option = Select.Option;
+
 const columns = [{
   title: 'Name',
   dataIndex: 'name',
@@ -48,51 +51,72 @@ const columns = [{
   ),
 }];
 
-const data = [{
-  key: '1',
-  name: 'Sukanta Sinha',
-  requirement: 'Lorem',
-  status: 'Pipeline',
-  technology: 'techniques',
-  estart: '13-04-2018',
-  astart: '14-04-2018',
-  expectedtask: '13-05-2018',
-  taskend: '14-05-2018',
+// const data = [{
+//   key: '1',
+//   name: 'Sukanta Sinha',
+//   requirement: 'Lorem',
+//   status: 'Pipeline',
+//   technology: 'techniques',
+//   estart: '13-04-2018',
+//   astart: '14-04-2018',
+//   expectedtask: '13-05-2018',
+//   taskend: '14-05-2018',
 
-}, {
-  key: '2',
-  name: 'Payel Dutta',
-  requirement: 'Lorem',
-  status: 'Pipeline',
-  technology: 'techniques',
-  estart: '15-04-2018',
-  astart: '09-04-2018',
-  expectedtask: '25-05-2018',
-  taskend: '28-05-2018',
+// }, {
+//   key: '2',
+//   name: 'Payel Dutta',
+//   requirement: 'Lorem',
+//   status: 'Pipeline',
+//   technology: 'techniques',
+//   estart: '15-04-2018',
+//   astart: '09-04-2018',
+//   expectedtask: '25-05-2018',
+//   taskend: '28-05-2018',
 
 
-}, {
-  key: '3',
-  name: 'Priyanka Saha',
-  requirement: 'Lorem',
-  status: 'Pipeline',
-  technology: 'techniques',
-  estart: '14-04-2018',
-  astart: '10-04-2018',
-  expectedtask: '23-05-2018',
-  taskend: '30-05-2018',
+// }, {
+//   key: '3',
+//   name: 'Priyanka Saha',
+//   requirement: 'Lorem',
+//   status: 'Pipeline',
+//   technology: 'techniques',
+//   estart: '14-04-2018',
+//   astart: '10-04-2018',
+//   expectedtask: '23-05-2018',
+//   taskend: '30-05-2018',
 
-}];
+// }];
+
 class ProjectlistView extends Component {
   constructor(props) {
     super(props);
     this.state = {
       projectList: [],
+      projectArray: [],
       page: 0,
       limit: 20,
       userId: sessionStorage.getItem('id'),
 
     }
+
+
+  }
+  handleChange = (value) => {
+    console.log(`selected ${value}`);
+    let projectArray;
+    if (value) {
+      if (value == 'All') {
+        this.setState({ projectArray: this.state.projectList });
+      }
+      else {
+        projectArray = this.state.projectList.filter(a => {
+          return a.status.indexOf(value) > -1
+        });
+        this.setState({ projectArray })
+        console.log("filtered data", this.state.projectArray);
+      }
+    }
+
   }
   componentWillMount() {
     this.viewProject();
@@ -103,25 +127,41 @@ class ProjectlistView extends Component {
     console.log('project List')
     this.props.projectList(this.state.userId, this.state.page, this.state.limit).then((sucess) => {
       console.log(sucess);
-      this.setState({ projectList:sucess.result });
-      // console.log(this.state.projectList)
+      this.setState({ projectList: sucess.result });
+      this.setState({ projectArray: sucess.result });
+      console.log(this.state.projectList)
     }, err => {
 
     });
   }
+
+
   render() {
-console.log('render')
+    console.log('render')
     return (
       <div className="projectListdiv">
         <h1 className="clientList">PROJECT LIST</h1>
+        <div>
+          <Select defaultValue="All" style={{ width: 120 }} onChange={this.handleChange}>
+            <Option value="All">All</Option>
+            <Option value="New">New</Option>
+            <Option value="InDiscussion">InDiscussion</Option>
+            <Option value="Scoping">Scoping</Option>
+            <Option value="InProgess">InProgess</Option>
+            <Option value="Stalled">Stalled</Option>
+            <Option value="Completed">Completed</Option>
+
+          </Select>
+        </div>
         <Row>
           <div className="addButton clientadd">
             <Button onClick={() => { this.props.history.push('/dashboard/newproject') }} >+</Button>
           </div>
+
         </Row>
         {/* clientlist */}
         <Card className="innercardContenta" bordered={false}>
-          <Table columns={columns} dataSource={this.state.projectList} />
+          <Table columns={columns} dataSource={this.state.projectArray} />
         </Card>
         {/* clientlist */}
       </div>
@@ -134,4 +174,4 @@ const mapStateToProps = (state) => {
 }
 // const WrappedProjectlistView 
 
-export default connect(mapStateToProps,actioncreators)(ProjectlistView);
+export default connect(mapStateToProps, actioncreators)(ProjectlistView);
