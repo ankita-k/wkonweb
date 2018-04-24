98316b34071c7c23be0f2b466f0be8bc5c10dbeb
Sukanta Sinha <Sukanta Sinha>
2018-04-24 10:49:51 +0530

    user list shown with data

--- src/components/Userlist/Userlist.js
+++ src/components/Userlist/Userlist.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import * as actioncreators from '../../redux/action';
+import { connect } from "react-redux";
 import './Userlist.css';
 import { Card, Table, Button, Icon, Row, Input, Col, Modal, span } from 'antd';
 import user from '../../Images/wkon-2-21.png';
 
 class Userlist extends Component {
+    constructor(props) {
+        console.log(props);
+        super(props);
+        this.getUser();
+        this.state = {
+            userList:[]
+        }
+    }
 
-
+    // get user list
+    getUser = () => {
+        this.props.userlist().then(result => {
+            console.log(result);
+            this.setState({userList: result.result});
+            console.log(this.state.userList)
+        })
+    }
     render() {
         return (
             <div className="userlist">
 
                 <h1>USER LIST</h1>
-
                 <div className="user1">
 
                     <Row>
+                    {this.state.userList.map((item,index) => {
+          return <div key={item._id}>
                         <Col lg={11} className="firstuser">
+                        
                             <Row className="btnedit">
-                            <Col lg={20}></Col>
+                                <Col lg={20}></Col>
                                 <Col lg={2}>
                                     <Button className="edit" >
                                         <a href="javascript:;"><Icon type="edit" /></a></Button></Col>
@@ -36,24 +54,28 @@ class Userlist extends Component {
                                 <Col lg={1}></Col>
                                 <Col lg={19}>
                                     <Col lg={12}>
-                                        <p><span className="span1">Name </span>: jhon</p>
+                                        <p><span className="span1">Name </span>:{item.name} </p>
 
-                                        <p><span className="span1">Phone </span>: 123456789</p>
+                                        <p><span className="span1">Phone </span>: {item.phoneNumber}</p>
 
                                     </Col>
                                     <Col lg={12}>
 
-                                        <p><span className="span1">Email</span>: jhon@mail.com</p>
+                                        <p><span className="span1">Email</span>: {item.email}</p>
 
-                                        <p><span className="span1">Roles </span>: Sales</p>
+                                        <p><span className="span1">Roles </span>: {item.role}</p>
                                     </Col>
-                                    <Row><p><span className="span1">Reporting Manager </span>: Pushpendu</p></Row></Col>
+                                    <Row><p><span className="span1">Reporting Manager </span>: {item.manager}</p></Row></Col>
                             </Row>
 
 
                         </Col>
+                        </div>
+                          })
+                        }
+                        
                         <Col lg={1}></Col>
-                        <Col lg={11} className="firstuser">
+                        {/* <Col lg={11} className="firstuser">
                             <Row className="padng20">
                                 <Col lg={4} className="resalign">
                                     <img src={user} />
@@ -76,11 +98,12 @@ class Userlist extends Component {
                             </Row>
 
 
-                        </Col>
+                        </Col> */}
+                        
                     </Row>
 
-                </div>
-                <div className="user1">
+                </div>         
+                {/* <div className="user1">
                     <Row>
                         <Col lg={11} className="firstuser">
                             <Row className="padng20">
@@ -133,7 +156,7 @@ class Userlist extends Component {
                         </Col>
                     </Row>
 
-                </div>
+                </div> */}
 
                 {/* <Col lg={12}>
 <Row>
@@ -154,5 +177,9 @@ class Userlist extends Component {
         );
     }
 }
+const mapStateToProps = (state) => {
+    return state
+}
 
-export default Userlist;
+// const  Userlist= Form.create()(NewProject);
+export default connect(mapStateToProps, actioncreators)(Userlist);
