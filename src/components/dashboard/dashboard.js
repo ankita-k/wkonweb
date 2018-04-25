23de6b0e034a3822512e493fe07328c6222e43e5
Pushpendu Ghosh <pushpendu.ghosh@memeinfotech.com>
2018-04-25 16:30:59 +0530

    changes

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -25,16 +25,33 @@ class Dashboard extends Component {
     this.state = {
       username: '',
       selectedKey: ['home'],
-      userrole:''
+      userrole: ''
     }
-    if(!sessionStorage.getItem('id') && !localStorage.getItem('id')){
+    if (!sessionStorage.getItem('id') && !localStorage.getItem('id')) {
       this.props.history.push('/login');
       return;
     }
-    
+
+  }
+
+  gotoDashboard = () => {
+    this.setState({
+      selectedKey: ['home']
+    });
+  }
+
+  handleClick = (nav) => {
+    let navArr = [];
+    navArr.push(nav.key);
+    this.setState({
+      selectedKey: navArr
+    });
   }
 
   renderSidemenuSelection = () => {
+    this.setState({
+      selectedKey: ['client_list']
+    });
     console.log(this.props.location.pathname);
     if (this.props.location.pathname != '/dashboard') {
       this.props.history.push('/dashboard');
@@ -52,7 +69,7 @@ class Dashboard extends Component {
         console.log(data);
         if (!data.error) {
           this.setState({ username: data.result.name });
-          this.setState({userrole:data.result.role})
+          this.setState({ userrole: data.result.role })
           console.log(this.state.username);
         }
 
@@ -60,14 +77,14 @@ class Dashboard extends Component {
 
       })
     }
-    else if(localStorage.getItem('id')){
+    else if (localStorage.getItem('id')) {
 
       this.props.username(localStorage.getItem('id')).then((data) => {
         console.log('data')
         console.log(data);
         if (!data.error) {
           this.setState({ username: data.result.name });
-          this.setState({userrole:data.result.role})
+          this.setState({ userrole: data.result.role })
           console.log(this.state.username);
         }
 
@@ -86,7 +103,7 @@ class Dashboard extends Component {
           <Header className="header">
             <Row>
               <Col lg={3}><NavLink to="../dashboard" activeClassName="active">
-                <img src={brandlogo} /></NavLink> </Col>
+                <img src={brandlogo} onClick={() => { this.gotoDashboard() }} /></NavLink> </Col>
               <p className="username" style={{ color: '#fff' }}> {this.state.username} <Button className="wkonlogout" onClick={() => {
                 sessionStorage.clear();
                 localStorage.clear();
@@ -97,9 +114,9 @@ class Dashboard extends Component {
           <Layout>
             <Sider width={200} style={{ background: '#fff' }}>
               <Menu
+                onClick={this.handleClick}
                 mode="inline"
-                defaultSelectedKeys={this.state.selectedKey}
-                defaultOpenKeys={this.state.selectedKey}
+                selectedKeys={this.state.selectedKey}
                 style={{ height: '100%', borderRight: 0 }}
               >
                 <Menu.Item key="home">
@@ -107,42 +124,42 @@ class Dashboard extends Component {
                   <span>Home</span>
                   <NavLink to="../dashboard" activeClassName="active"></NavLink>
                 </Menu.Item>
-              {this.state.userrole=="admin"||"Sales"?
-                <SubMenu  key="client" title={<span>Clients</span>} subMenuCloseDelay={0.1}>
-                  <Menu.Item key="create_client">
-                    <span>Client Create</span>
-                    <NavLink to="../dashboard/clientcreate" activeClassName="active"></NavLink>
-                  </Menu.Item>
-                  <Menu.Item key="client_list">
-                    <span>Client List</span>
-                    <NavLink to="../dashboard/clientlist" activeClassName="active"></NavLink>
-                  </Menu.Item>
-                </SubMenu>:''}
-                {this.state.userrole=="Developer"||"admin"?
-                <SubMenu key="projects" title={<span>Projects</span>} subMenuCloseDelay={0.1}>
-                  <Menu.Item key="create_project">
-                    <span>Project Create</span>
-                    <NavLink to="../dashboard/newproject" activeClassName="active"></NavLink>
-                  </Menu.Item>
-                  <Menu.Item key="project_list">
-                    <span>Project List</span>
-                    <NavLink to="../dashboard/projectlist" activeClassName="active"></NavLink>
-                  </Menu.Item>
-                </SubMenu>:''}
-                {this.state.userrole=="admin"?
-                <SubMenu key="user" title={<span>User Management</span>} subMenuCloseDelay={0.1}>
-                  <Menu.Item key="create_user">
-                    <span>Create User</span>
-                    <NavLink to="../dashboard/createuser" activeClassName="active"></NavLink>
-                  </Menu.Item>
-                  <Menu.Item key="user_list">
-                    <span>User List</span>
-                    <NavLink to="../dashboard/userlist" activeClassName="active"></NavLink>
-                  </Menu.Item>
-                </SubMenu>:''}
+                {this.state.userrole == "admin" || "Sales" ?
+                  <SubMenu key="client" title={<span>Clients</span>} subMenuCloseDelay={0.1}>
+                    <Menu.Item key="create_client">
+                      <span>Client Create</span>
+                      <NavLink to="../dashboard/clientcreate" activeClassName="active"></NavLink>
+                    </Menu.Item>
+                    <Menu.Item key="client_list">
+                      <span>Client List</span>
+                      <NavLink to="../dashboard/clientlist" activeClassName="active"></NavLink>
+                    </Menu.Item>
+                  </SubMenu> : ''}
+                {this.state.userrole == "Developer" || "admin" ?
+                  <SubMenu key="projects" title={<span>Projects</span>} subMenuCloseDelay={0.1}>
+                    <Menu.Item key="create_project">
+                      <span>Project Create</span>
+                      <NavLink to="../dashboard/newproject" activeClassName="active"></NavLink>
+                    </Menu.Item>
+                    <Menu.Item key="project_list">
+                      <span>Project List</span>
+                      <NavLink to="../dashboard/projectlist" activeClassName="active"></NavLink>
+                    </Menu.Item>
+                  </SubMenu> : ''}
+                {this.state.userrole == "admin" ?
+                  <SubMenu key="user" title={<span>User Management</span>} subMenuCloseDelay={0.1}>
+                    <Menu.Item key="create_user">
+                      <span>Create User</span>
+                      <NavLink to="../dashboard/createuser" activeClassName="active"></NavLink>
+                    </Menu.Item>
+                    <Menu.Item key="user_list">
+                      <span>User List</span>
+                      <NavLink to="../dashboard/userlist" activeClassName="active"></NavLink>
+                    </Menu.Item>
+                  </SubMenu> : ''}
                 {/* <Menu.Item key="8"><NavLink to="../dashboard/usermanagement">User Management</NavLink></Menu.Item>
                 <Menu.Item key="9"><NavLink to="../dashboard/userlist">User List</NavLink></Menu.Item> */}
-                
+
 
                 {/* <SubMenu key="sub1" title={<span><Icon type="home" />Home</span>}> */}
                 {/* <Menu.Item key="1">Clients<NavLink to="../dashboard/clientlist" activeClassName="active">Clients</NavLink></Menu.Item>
@@ -163,7 +180,7 @@ class Dashboard extends Component {
                 <Route exact path={`${this.props.match.url}/clientlist`} component={ClientList} />
                 <Route exact path={`${this.props.match.url}/createuser`} component={UserManagement} />
                 <Route exact path={`${this.props.match.url}/edituser`} component={UserManagement} />
-                <Route exact path={`${this.props.match.url}/userlist`} component={Userlist} />   
+                <Route exact path={`${this.props.match.url}/userlist`} component={Userlist} />
                 {/* <DashboardView></DashboardView> */}
                 {/* <NewInformation></NewInformation> */}
               </Content>
