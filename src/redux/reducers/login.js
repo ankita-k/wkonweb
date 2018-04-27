8fd9493e8c4420e51d5d5ed4b4445fec3a1ace0a
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-04-27 10:11:39 +0530

    tost shown in login with new reducer

--- src/redux/reducers/login.js
+++ src/redux/reducers/login.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import * as actionCreators from '../action';
 
-//LOGIN REDUCER
+//LOGIN REDUCER FOR USER LOGIN
 export function loginReducer (state = {}, action) {
   
     switch (action.type) {
        
         case 'USER_LOGIN_SUCCESS':
-          return {userInfo:action.json.result}
+          return {userInfo:action.json.result}   //returning user info 
             break;
         default:
             return state
