ae2e4b2d04385acc9b6c664b19e4e0db6a761ea1
Jyoti-meme <jyoti.mala@memeinfotech.com>
2018-05-03 16:24:44 +0530

    list fetched in appjs

--- src/components/ClientList/ClientList.js
+++ src/components/ClientList/ClientList.js
@@ -123,7 +123,7 @@ class ClientList extends Component {
       searchedclient: [],
       trueClientList: [],
       searchinput: '',
-      c: 'All',
+      allclient: 'All',
       userId: sessionStorage.getItem('id') ? sessionStorage.getItem('id') : localStorage.getItem('id'),
       column: [{
         title: 'Name',
@@ -170,6 +170,28 @@ class ClientList extends Component {
     }
   }
 
+  componentDidMount() {
+    console.log('component will mount');
+    this.setState({ show: true });
+    this.commonFunction();
+  }
+
+  componentWillReceiveProps(props) {
+    console.log(props);
+    this.commonFunction();
+
+  }
+
+  // COMMON FUNCTION FOR PROPS FOR COMPONENT DID MOUNT AND COMPONENT WILL RECEIVE PROPS
+  commonFunction() {
+    /* SHOWING CLIENT LIST AFTER RECEIVING DATA FROM PROPS*/
+    if (this.props.clientList.length > 0) {
+      this.setState({ searchedclient: (this.props.clientList) });
+      this.setState({ show: false });
+    }
+    this.handleChange((this.props.location.filterValue));
+  }
+  
   //edit client
   editClient = (data) => {
     this.props.history.push({
@@ -202,25 +224,9 @@ class ClientList extends Component {
 
   }
 
-  componentDidMount() {
-    console.log('component will mount');
-    this.setState({ show: true });
-    /* GETTING CLIENT LIST
-    */
-    this.props.actions.clientlist(this.state.userId);    
-  }
+ 
 
-  componentWillReceiveProps(props) {
-    console.log(props);
-
-    /* SHOWING CLIENT LIST AFTER RECEIVING DATA FROM PROPS*/
-    if (props.clientList.length > 0) {
-      this.setState({ show: false });
-      this.setState({ searchedclient: props.clientList })
-    }
-
-  }
-
+  
 
   // SEACRH CLIENT LIST ACCORDING TO INPUT 
   searchClient = (e) => {
@@ -243,14 +249,14 @@ class ClientList extends Component {
       });
     }
   }
-  
+
   //handlechange function
   handleChange = (value) => {
     // let searchedclient = [];
     if (value != null || value != undefined) {
       this.setState({ statussearch: value })
       if (value == 'All') {
-        this.setState({ statussearch: this.state.c });
+        this.setState({ statussearch: 'All' });
         this.setState({ searchinput: '' });
         this.setState({
           searchedclient: this.props.clientList
@@ -291,51 +297,51 @@ class ClientList extends Component {
           color="red"
           showSpinner={false}
         />
-          <div className="projectListheader">
-        <h1 className="clientList">Client List</h1>
-        <Row>
-          <div className="addButton clientadd">
-            <Button onClick={() => { this.props.history.push('/dashboard/clientcreate') }}>+</Button>
-          </div>
-        </Row>
-        <Row>
-          <div className="AllProjects">
-            <Search className="SearchValue"
-              placeholder="input search text"
-              onSearch={(value) => { this.searchClient(value) }}
-              style={{ width: 200 }}
-              onChange={(e) => { this.showallList(e.target.value) }}
-              enterButton
-              value={this.state.searchinput}
-            />
-            {(this.state.statussearch) ?
-              <Select className="scoping" value={this.state.statussearch} style={{ width: 120 }} onChange={this.handleChange}>
-                <Option value="All">All</Option>
-                <Option value="Interested">Interested</Option>
-                <Option value="Pipeline">Pipeline</Option>
-                <Option value="Commited">Committed</Option>
+        <div className="projectListheader">
+          <h1 className="clientList">Client List</h1>
+          <Row>
+            <div className="addButton clientadd">
+              <Button onClick={() => { this.props.history.push('/dashboard/clientcreate') }}>+</Button>
+            </div>
+          </Row>
+          <Row>
+            <div className="AllProjects">
+              <Search className="SearchValue"
+                placeholder="input search text"
+                onSearch={(value) => { this.searchClient(value) }}
+                style={{ width: 200 }}
+                onChange={(e) => { this.showallList(e.target.value) }}
+                enterButton
+                value={this.state.searchinput}
+              />
+              {(this.state.statussearch) ?
+                <Select className="scoping" value={this.state.statussearch} style={{ width: 120 }} onChange={this.handleChange}>
+                  <Option value="All">All</Option>
+                  <Option value="Interested">Interested</Option>
+                  <Option value="Pipeline">Pipeline</Option>
+                  <Option value="Commited">Committed</Option>
 
 
-              </Select> :
-              <Select className="scoping" defaultValue="All" style={{ width: 120 }} onChange={this.handleChange}>
-                <Option value="All">All</Option>
-                <Option value="Interested">Interested</Option>
-                <Option value="Pipeline">Pipeline</Option>
-                <Option value="Commited">Committed</Option>
+                </Select> :
+                <Select className="scoping" defaultValue="All" style={{ width: 120 }} onChange={this.handleChange}>
+                  <Option value="All">All</Option>
+                  <Option value="Interested">Interested</Option>
+                  <Option value="Pipeline">Pipeline</Option>
+                  <Option value="Commited">Committed</Option>
 
 
-              </Select>
-            }
-            <Button className="allprojectbtn" onClick={() => {
-              this.handleChange('All')
-            }}>Show All</Button>
+                </Select>
+              }
+              <Button className="allprojectbtn" onClick={() => {
+                this.handleChange('All')
+              }}>Show All</Button>
 
-          </div>
-        </Row>
-</div>
+            </div>
+          </Row>
+        </div>
         {/* clientlist */}
         <Card className="innercardContenta" bordered={false}>
-          <Table 
+          <Table
             onRow={(record) => {
               return {
                 onClick: () => { console.log(record), this.setState((prevstate) => { return { selectedId: record } }) },       // click row
--- src/redux/reducers/projectlist.js
+++ src/redux/reducers/projectlist.js
@@ -1,13 +1,31 @@
 import { createStore, applyMiddleware } from 'redux';
 import * as actionCreators from '../action';
+import moment from 'moment';
 
 //LOGIN REDUCER FOR PROJECTLIST
-export function projectList (state = {}, action) {
+export function projectList (state = [], action) {
   
     switch (action.type) {
        
         case 'PROJECT_LIST':
-          return {projectlist:action.json.result}   // returning project list array
+          return action.list.map(function (item, index) {
+            return {
+                name: item.name.length > 20 ? (item.name.slice(0, 20) + '...') : item.name,
+                name1: item.name,
+                requirement: item.requirement.length > 15 ? (item.requirement.slice(0, 15) + '...') : item.requirement,
+                requirement1: item.requirement,
+                status: item.status,
+                technology: (item.technology.replace(/"/g, '')).split(',').length > 1 ? ((item.technology.replace(/"/g, '')).split(',')[0] + '..') : (item.technology.replace(/"/g, '')).split(','),
+                technology1: (item.technology.replace(/"/g, '')).split(','),
+                expectedStartDate: item.expectedStartDate ? moment(item.expectedStartDate).format("ll") : '',
+                expectedEndDate: item.expectedEndDate ? moment(item.expectedEndDate).format("ll") : '',
+                actualStartDate: item.actualStartDate ? moment(item.actualStartDate).format("ll") : '',
+                actualEndDate: item.actualEndDate ? moment(item.actualEndDate).format("ll") : '',
+                key: Math.random() * 1000000000000000000,
+                _id: item._id,
+                client: item.client
+            }})
+                   // returning project list array
             break;
         default:
             return state
