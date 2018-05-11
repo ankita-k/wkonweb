49cb6eec470a8aac3a656cbe6e879b6cc7e4db3d
supryodasmeme <supryo.das@memeinfotech.com>
2018-05-11 17:12:21 +0530

    productmanagement page ui fixed

--- src/components/projectManagement/projectManagement.js
+++ src/components/projectManagement/projectManagement.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './projectManagement.css';
 import modulelogo from '../../Images/module.svg';
 import modulesidebarlogo from '../../Images/module2.svg';
-import { Layout, Input, Menu, Row, Col, List, Avatar, Form, Select, Dropdown, Button, Icon } from 'antd';
+import { Layout, Modal, Input, Menu, Row, Col, List, Avatar, Form, Select, Dropdown, Button, Icon, Breadcrumb } from 'antd';
+import backbtn from '../../Images/backbtn.svg';
+import addbtn from '../../Images/addbtn.svg';
 const Option = Select.Option;
 const Search = Input.Search;
 const FormItem = Form.Item;
+const { TextArea } = Input;
 const data = [
     {
         title: 'Lorem Ipsum',
@@ -20,38 +23,57 @@ const data = [
         title: 'Lorem Ipsum',
     },
 ];
-const menu = (
-    <Menu>
-        <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">1st menu item</a>
-        </Menu.Item>
-        <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">2nd menu item</a>
-        </Menu.Item>
-        <Menu.Item>
-            <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">3rd menu item</a>
-        </Menu.Item>
-    </Menu>
-);
+
 
 const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
 
 function handleChange(value) {
-  console.log(`selected ${value}`);
+    console.log(`selected ${value}`);
 }
 
 function handleBlur() {
-  console.log('blur');
+    console.log('blur');
 }
 
 function handleFocus() {
-  console.log('focus');
+    console.log('focus');
 }
 
 class ProjectManagement extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            modal2Visible: false,
+            menu:(
+                <Menu>
+                    <Menu.Item>
+                        <a onClick={() => this.setModal2Visible(true)}>Module</a>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <a onClick={() => this.setModal3Visible(true)}>Sub Module</a>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <a onClick={() => this.setModal4Visible(true)}>Task</a>
+                    </Menu.Item>
+                </Menu>
+            )
+        }
+    }
+
+
+
+    setModal2Visible = (modal2Visible) => {
+        this.setState({ modal2Visible });
+    }
+    setModal3Visible = (modal3Visible) => {
+        this.setState({ modal3Visible });
+    }
+    setModal4Visible = (modal4Visible) => {
+        this.setState({ modal4Visible });
+    }
     render() {
         const { size } = this.props;
         const state = this.state;
@@ -61,18 +83,36 @@ class ProjectManagement extends Component {
             <div className="projectManagementpanda">
                 <Layout>
                     <Row>
-                        <Col lg={4}>
+                        <Col lg={12}>
                             <div className="wkonList sidewkonlist">
                                 <Row>
-                                    <div className="listView">
-                                        <Search
-                                            placeholder="Type to filter"
-                                            onSearch={value => console.log(value)}
-                                            style={{ width: 200 }}
-                                        />
-                                        {/* <div classname="settingbtn">
-                                      <Button type="default">  <Icon type="setting" /></Button>
-                                      </div> */}
+                                    <div className="listHeader">
+                                        <Row>
+                                            <Col lg={4}>
+                                                <Button type="primary"><img src={backbtn} /></Button>
+                                            </Col>
+                                            <Col lg={12}>
+                                                <Breadcrumb>
+                                                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                                    <Breadcrumb.Item><a href="">Module</a></Breadcrumb.Item>
+                                                    <Breadcrumb.Item><a href="">Sub_module</a></Breadcrumb.Item>
+                                                    <Breadcrumb.Item>Project</Breadcrumb.Item>
+                                                </Breadcrumb>
+                                            </Col>
+
+                                            <Col lg={6}></Col>
+                                            <Col lg={2}>
+
+                                                <div className="listaddbtn">
+                                                    <Dropdown overlay={this.state.menu} placement="bottomCenter" trigger={['click']}>
+                                                        <Button><img className="plus" src={addbtn} /></Button>
+                                                    </Dropdown>
+                                                </div>
+                                            </Col>
+
+                                        </Row>
+
+
                                     </div>
                                 </Row>
                                 <List
@@ -90,138 +130,94 @@ class ProjectManagement extends Component {
                                 />
                             </div>
                         </Col>
-                        <Col lg={8}>
-                            <div className="wkonList secondlist">
-                                <Row>
-                                    <div className="listView">
-                                        <Dropdown overlay={menu} placement="bottomLeft">
-                                            <Button>Home</Button>
-                                        </Dropdown>
 
-                                    </div>
-                                </Row>
-                                <List
-                                    itemLayout="horizontal"
-                                    dataSource={data}
-                                    renderItem={item => (
-                                        <List.Item>
-                                            <List.Item.Meta
-                                                avatar={<Avatar src={modulelogo} />}
-                                                title={<a href="#">{item.title}</a>}
-                                                description="Lorem Ipsum is simply dummy text"
-                                            />
-                                        </List.Item>
-                                    )}
-                                />
-                            </div>
-                        </Col>
                         <Col lg={12}>
-                            <div className="detailview">
-                                <div className="projectpreview">
-                                    <Row className="projectname">
-                                        <h1>Project Name</h1>
-                                    </Row>
-                                    <Row>
-                                        <Col xs={24} sm={24} md={24} lg={12}>
-                                            <p className="expecteDateclient">Assign to :</p>
-                                            <FormItem>
+                            <div className="wkonList detailView">
+                                <div className="projectname">
+                                    <p>Project Name :</p>
+                                    <Input placeholder="" />
 
-
-
-                                                <Select
-                                                    showSearch
-                                                    // style={{ width: 200 }}
-                                                    placeholder="Select a person"
-                                                    optionFilterProp="children"
-                                                    onChange={handleChange}
-                                                    onFocus={handleFocus}
-                                                    onBlur={handleBlur}
-                                                    filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                                                >
-                                                    <Option value="jack">Pushpendu</Option>
-                                                    <Option value="lucy">payel</Option>
-                                                    <Option value="tom">Tom</Option>
-                                                </Select>
-
-                                            </FormItem>
-                                        </Col>
-                                        <Col xs={24} sm={24} md={24} lg={12}>
-                                            <p className="expecteDateclient">Status :</p>
-                                            <FormItem>
-
-
-
-                                                <Select
-                                                    showSearch
-                                                    // style={{ width: 200 }}
-                                                    placeholder="Select a person"
-                                                    optionFilterProp="children"
-                                                    onChange={handleChange}
-                                                    onFocus={handleFocus}
-                                                    onBlur={handleBlur}
-                                                    filterOption={(input, option) => option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
-                                                >
-                                                    <Option value="jack">Active</Option>
-                                                    <Option value="lucy">Pending</Option>
-                                                    <Option value="tom">Tom</Option>
-                                                </Select>
-                                                    
-                                                
-                                         
-                                        </FormItem>
-                                    </Col>
-                                    </Row>
-                                <Row>
-                                    <div className="startbtn">
-                                        <Button  type="default">Start project</Button>
-
-                                        <Button type="primary">End project</Button>
-                                    </div>
-                                </Row>
-                                {/* <Row className="asign"><p>Asign to</p></Row> */}
-                                {/* <Row>
-                                        <div className="asignTo">
-                                        <p>Asign to :</p>
-                                            <Select
-                                                value={state.currency}
-                                                size={size}
-                                                // style={{ width: '3%' }}
-                                                onChange={this.handleCurrencyChange}
-                                            >
-                                                <Option value="rmb">Pushpendu</Option>
-                                                <Option value="dollar">Payel</Option>
-                                            </Select>
-                                        </div>
-                                    </Row>
-                                    <Row>
-                                        <div className="startbtn">
-                                            <Button type="default">Start project</Button>
-                                        </div>
-                                   
-                                        <div className="endbtn">
-                                            <Button type="default">End project</Button>
-                                        </div>
-                                    </Row>
-                                    {/* <Row className="statusreport"></Row> */}
-                                {/* <Row>
-                                        <div className="status">
-                                        <p>Status</p>
-                                        <Select
-                                                value={state.currency}
-                                                size={size}
-                                                style={{ width: '90%' }}
-                                                onChange={this.handleCurrencyChange}
-                                            >
-                                                <Option value="rmb">Active</Option>
-                                                <Option value="dollar">Pending</Option>
-                                            </Select>
-                                        </div>
-                                    </Row> */}
-                            </div>
+                                </div>
+                                <div className="projectdata">
+                                    <p>Project Details :</p>
+                                    <TextArea rows={4} />
+                                </div>
+                                <div className="savebtn">
+                                    <Button>Save</Button>
+                                </div>
                             </div>
                         </Col>
                     </Row>
+                    <div className="modal"><Modal
+                        title="Module name"
+                        wrapClassName="Module name"
+                        visible={this.state.modal2Visible}
+                        onOk={() => this.setModal2Visible(false)}
+                        onCancel={() => this.setModal2Visible(false)}
+                    >
+                      <div className="projectname">
+                                    <p>Name :</p>
+                                    <Input placeholder="" />
 
+                                </div>
+                                <div className="projectdata">
+                                    <p>Details :</p>
+                                    <TextArea rows={4} />
+                                </div>
+                                <div className="savebtn modalbtn">
+                                    <Button>Save</Button>
+                                    <Button className="cancelbtn">Cancel</Button>
+                                </div>
+                    </Modal>
+                    </div>
+
+                    
+                    <div className="modal"><Modal
+                        title="New Sub module"
+                        wrapClassName="New Sub module"
+                        visible={this.state.modal3Visible}
+                        onOk={() => this.setModal3Visible(false)}
+                        onCancel={() => this.setModal3Visible(false)}
+                    >
+                       <div className="projectname">
+                                    <p>Name :</p>
+                                    <Input placeholder="" />
+
+                                </div>
+                                <div className="projectdata">
+                                    <p>Descriptions :</p>
+                                    <TextArea rows={4} />
+                                </div>
+                                <div className="savebtn modalbtn">
+                                    <Button>Save</Button>
+                                    <Button className="cancelbtn">Cancel</Button>
+                                </div>
+                        
+                    </Modal>
+                    </div>
+                    <div className="modal"><Modal
+                        title="Task"
+                        wrapClassName="Task"
+                        visible={this.state.modal4Visible}
+                        onOk={() => this.setModal4Visible(false)}
+                        onCancel={() => this.setModal4Visible(false)}
+                    >
+                        <div className="projectname">
+                                    <p>Name :</p>
+                                    <Input placeholder="" />
+
+                                </div>
+                                <div className="projectdata">
+                                    <p>Descriptions :</p>
+                                    <TextArea rows={4} />
+                                </div>
+                                <div className="savebtn modalbtn">
+                                    <Button>Save</Button>
+                                    <Button className="cancelbtn">Cancel</Button>
+                                </div>
+                        
+                    </Modal>
+                    </div>
 
                 </Layout>
             </div >
